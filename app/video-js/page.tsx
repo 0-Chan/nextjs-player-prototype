39502c21f568a9bc48b3677ef29c623af25057ff
0Chan-smc <cody@thesmc.co.kr>
2024-10-29 00:20:20 +0900

    feat: Add video.js player

--- app/video-js/page.tsx
+++ app/video-js/page.tsx
@@ -5,29 +5,28 @@ import { useRef } from "react";
 import videojs from "video.js";
 import VideoJS from "../../components/players/videoJs";
 
-
 export default function Home() {
   const playerRef = useRef(null);
 
   const videoJsOptionsM3u8 = {
     controls: true,
     autoplay: false,
-    width: 400,
+    width: 1280,
     sources: [
       {
-        src: 'https://ik.imagekit.io/ikmedia/sample-video.mp4/ik-master.m3u8?tr=sr-240_360_480_720',
-        type: 'application/x-mpegURL'
+        src: "https://ik.imagekit.io/ikmedia/sample-video.mp4/ik-master.m3u8?tr=sr-240_360_480_720",
+        type: "application/x-mpegURL",
       },
     ],
   };
   const videoJsOptionsMpd = {
     controls: true,
     autoplay: false,
-    width: 400,
+    width: 1280,
     sources: [
       {
-        src: 'https://ik.imagekit.io/demo/sample-video.mp4/ik-master.mpd?tr=sr-240_360_480_720_1080',
-        type: 'application/dash+xml'
+        src: "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd",
+        type: "application/dash+xml",
       },
     ],
   };
@@ -35,22 +34,21 @@ export default function Home() {
   const handlePlayerReady = (player: any) => {
     playerRef.current = player;
 
-    console.log(player.qualityLevels())
+    console.log(player.qualityLevels());
 
     // You can handle player events here, for example:
-    player.on('waiting', () => {
-      videojs.log('player is waiting');
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
     });
 
-    player.on('dispose', () => {
-      videojs.log('player will dispose');
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
     });
   };
   return (
-
-    <main >
+    <main>
       <div>
-        <VideoJS options={videoJsOptionsM3u8} onReady={handlePlayerReady} />
+        <VideoJS options={videoJsOptionsMpd} onReady={handlePlayerReady} />
       </div>
     </main>
   );
